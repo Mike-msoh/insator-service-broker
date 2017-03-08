d51790780775f9d4342323ab28f518f6bd485d23
msoh <ohms@kr.ibm.com>
2017-03-08 14:17:38 +0900

    new code

--- service.py
+++ service.py
@@ -19,7 +19,7 @@ def insatorsvc():
 	        'redirect_uri' : 'http://bluemix.net'
 	    },
 	    'metadata' : {
-	        'displayName' : 'Nexsign Service',
+	        'displayName' : 'Insator Service',
 	        'serviceMonitorApi' : 'https://cf-upsi-app.mybluemix.net/healthcheck',
 	        'providerDisplayName' : 'S.D.S',
 	        'longDescription' : 'Write full description of insator service',
@@ -33,13 +33,13 @@ def insatorsvc():
 	                'description' :' Insator Service is built to let you connect easily and to all of your services and applications. You can start analyzing your data immediately with familiar tools.'
 	            }
 	        ],
-	        # 'featuredImageUrl' : 'http://insator-ui-service.mybluemix.net/images/icon_64x64.png',
-	        # 'imageUrl' : 'http://insator-ui-service.mybluemix.net/images/images/50x50.png',
-	        # 'mediumImageUrl' : 'http://insator-ui-service.mybluemix.net/images/32x32.png',
-	        # 'smallImageUrl' : 'http://insator-ui-service.mybluemix.net/images/24x24.png',
-	        # 'documentationUrl' : 'http://www.samsungsds.com/...html',
-	        # 'instructionsUrl' : 'http://www.samsungsds.com/...html',
-	        # 'termsUrl' : 'https://media.termsfeed.com/pdf/terms-and-conditions-template.pdf'
+	        'featuredImageUrl' : 'http://fido-ui-service.mybluemix.net/images/nexsign/Nexsign_icon_64x64.png',
+	        'imageUrl' : 'http://fido-ui-service.mybluemix.net/images/images/nexsign/Nexsign_icon_50x50.png',
+	        'mediumImageUrl' : 'http://fido-ui-service.mybluemix.net/images/nexsign/Nexsign_icon_32x32.png',
+	        'smallImageUrl' : 'http://fido-ui-service.mybluemix.net/images/nexsign/Nexsign_icon_24x24.png',
+	        'documentationUrl' : 'http://www.samsungsds.com/us/en/solutions/off/nex/nexsign.html',
+	        'instructionsUrl' : 'http://www.samsungsds.com/us/en/solutions/off/nex/nexsign.html',
+	        'termsUrl' : 'https://media.termsfeed.com/pdf/terms-and-conditions-template.pdf'
 	    }
 	}
 	return insator_service
